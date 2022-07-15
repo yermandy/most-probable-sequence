32db1840802a7d6142636d62b3169170c34b1f12
yermandy <yermaand@gmail.com>
2022-07-15 17:07:18 +0200

    tests

--- compare_dp_ilp.py
+++ compare_dp_ilp.py
@@ -2,20 +2,25 @@ from dp import *
 from utils import generate_random
 
 
-f, y = generate_random(0)
-
-n = f.shape[0]
-Y = f.shape[1]
-
-C_max = (Y - 1) * (n + 1)
-
-F, Is = dymanic_programming(f, n, Y)
-G, s, t = create_graph(f)
-
-for c in range(1, C_max + 2):
-    objective, maximizers = backtrack(Is, F, c, Y)
+for seed in range(0, 100):
     
-    print(c - 1, objective, maximizers, '<- DP')
-    print(c - 1, *evaluate(f, G, s, t, c - 1), '<- ILP')
-    
-    print()
+    f, y = generate_random(seed)
+
+    n = f.shape[0]
+    Y = f.shape[1]
+
+    C_max = (Y - 1) * (n + 1)
+
+    F, Is = dymanic_programming(f, n, Y)
+    G, s, t = create_graph(f)
+
+    for c in range(1, C_max + 2):
+        objective_ilp, maximizers_ilp = evaluate(f, G, s, t, c - 1)
+        objective_dp, maximizers_dp = backtrack(Is, F, c, Y)
+        
+        print(c - 1, objective_dp, maximizers_dp, '<- DP')
+        print(c - 1, *evaluate(f, G, s, t, c - 1), '<- ILP')
+        print()
+        
+        assert objective_dp - objective_ilp < 1e-6
+        assert np.all(maximizers_ilp == maximizers_dp)
