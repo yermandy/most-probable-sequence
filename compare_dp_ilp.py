8c16fe913c958c49bfebd8c7c9e6fb86d7664838
yermandy <yermaand@gmail.com>
2022-07-20 14:04:53 +0200

    compare ilp dp

--- compare_dp_ilp.py
+++ compare_dp_ilp.py
@@ -9,23 +9,19 @@ for seed in range(0, 100):
     n = f.shape[0]
     Y = f.shape[1]
 
-    C_max = (Y - 1) * (n + 1)
+    C_max = (Y - 1) * (n)
 
     F, Is = dymanic_programming(f, n, Y)
     G, s, t = create_graph(f)
     
     print('seed:', seed)
 
-    for c in range(1, C_max + 2):
-        try:
-            objective_dp, maximizers_dp = backtrack(Is, F, c, Y)
-        except:
-            print('dp failed')
-            continue
-        objective_ilp, maximizers_ilp = evaluate(f, G, s, t, c - 1)
+    for c in range(0, C_max + 1):
+        objective_dp, maximizers_dp = backtrack(Is, F, c, Y)
+        objective_ilp, maximizers_ilp = evaluate(f, G, s, t, c)
         
-        print(c - 1, objective_dp, maximizers_dp, '<- DP')
-        print(c - 1, objective_ilp, maximizers_ilp, '<- ILP')
+        print(c, objective_dp, maximizers_dp, '<- DP')
+        print(c, objective_ilp, maximizers_ilp, '<- ILP')
         print()
         
         # ? assert that DP objective calculated correctly
@@ -33,10 +29,13 @@ for seed in range(0, 100):
         assert longest_path - objective_dp < 1e-8, f'{longest_path} != {objective_dp}'
         
         # ? assert that DP objective is feasible
-        assert c - 1 == maximizers_dp.sum(), f'{c - 1} != {maximizers_dp.sum()}'
+        assert c == maximizers_dp.sum(), f'{c} != {maximizers_dp.sum()}'
         
-        # ? assert that DP maximizers are equal to ILP maximizers
-        assert np.all(maximizers_ilp == maximizers_dp)
-        
-        # ? assert that DP objective is equal to ILP objective
-        assert objective_dp - objective_ilp < 1e-8, f'{objective_dp} != {objective_ilp}'
+        # ! ILP objective can be lower than DP objective
+        if objective_dp <= objective_ilp:
+            
+            # ? assert that DP maximizers are equal to ILP maximizers
+            assert np.all(maximizers_ilp == maximizers_dp)
+            
+            # ? assert that DP objective is equal to ILP objective
+            assert objective_dp - objective_ilp < 1e-8, f'{objective_dp} != {objective_ilp}'
