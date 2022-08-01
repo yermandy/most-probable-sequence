b4a5d948e7b0c1e7a9d5bab1fcc63e026340f68a
yermandy <yermaand@gmail.com>
2022-08-01 15:40:55 +0200

    arguments

--- arguments.py
+++ arguments.py
@@ -1,11 +1,12 @@
 import argparse
 
-parser = argparse.ArgumentParser(description='x.py')
+parser = argparse.ArgumentParser()
 
+parser.add_argument('--split', type=int, default=0, help='split')
 parser.add_argument('--batch_size', type=int, default=1, help='batch size')
 parser.add_argument('--epochs', type=int, default=1000, help='number of epochs')
 parser.add_argument('--lr', type=float, default=0.01, help='learning rate')
-parser.add_argument('--weight_decay', type=float, default=0, help='weight decay')
+parser.add_argument('--weight_decay', type=float, default=0.0, help='weight decay')
 parser.add_argument('--seed', type=int, default=42, help='seed')
 parser.add_argument('--Y', type=int, default=6, help='Y - number of events in small window')
 parser.add_argument('--validation', type=bool, action=argparse.BooleanOptionalAction, default=True, help='use validation set')
